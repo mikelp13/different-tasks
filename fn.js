d0255981928b7d4a595812e45860405b09bb8adf
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2020-11-15 11:00:25 +0200

    update fn file

--- fn.js
+++ fn.js
@@ -122,35 +122,35 @@
 
 // ------------------------------ task-04 ----------------------------
 
-// const account = {
-//   owner: 'Mango',
-//   balance: 24000,
-//   discount: 0.1,
-//   orders: ['order-1', 'order-2', 'order-3'],
-//   changeDiscount(value) {
-//     discount = value; // Write code in this line
-//   },
-//   showOrders() {
-//     return orders; // Write code in this line
-//   },
-//   addOrder(cost, order) {
-//     balance -= cost; // Write code in this line
-//     orders.push(order); // Write code in this line
-//   },
-// };
-// const copyAccount =  Object.assign({},account);
-// copyAccount.orders = [...account.orders];
-// // копируем для автотестов ссылочные типы
+const account = {
+  owner: 'Mango',
+  balance: 24000,
+  discount: 0.1,
+  orders: ['order-1', 'order-2', 'order-3'],
+  changeDiscount(value) {
+    this.discount = value; // Write code in this line
+  },
+  showOrders() {
+    return this.orders; // Write code in this line
+  },
+  addOrder(cost, order) {
+    this.balance -= cost; // Write code in this line
+    this.orders.push(order); // Write code in this line
+  },
+};
+const copyAccount =  Object.assign({},account);
+copyAccount.orders = [...account.orders];
 
 
-// account.changeDiscount(0.15);
-// //console.log(account.discount); // 0.15
 
-// //console.log(account.showOrders()); 
-// // ['order-1', 'order-2', 'order-3']
+account.changeDiscount(0.15);
+console.log(account.discount); // 0.15
 
-// account.addOrder(5000, 'order-4');
-// //console.log(account.balance); // 19000
+console.log(account.showOrders()); 
+// ['order-1', 'order-2', 'order-3']
 
-// //console.log(account.showOrders());
-// // ['order-1', 'order-2', 'order-3', 'order-4']
+account.addOrder(5000, 'order-4');
+console.log(account.balance); // 19000
+
+console.log(account.showOrders());
+// ['order-1', 'order-2', 'order-3', 'order-4']
