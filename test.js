ef4a37a8fe659e68bf1c59709ae750c1d8317a68
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2020-11-19 15:24:31 +0200

    update test file

--- test.js
+++ test.js
@@ -1,1 +1,30 @@
+// find vowels leter
+
+const string = 'lorem spam find you';
+
+const searchLiteral = function (string) {
+  const LITERAL = 'oeaiu';
+  let total = 0;
+  const wordsArr = string.split(' ');
+  const symbolsArr = LITERAL.split('');
+  console.log(symbolsArr);
+
+  for (const word of wordsArr) {
+    let letters = word.split('');
+    for (const letter of letters) {
+      let symbol = LITERAL.includes(letter);
+
+      if(symbol){
+       total+=1;
+      }
+    }
+  }
+  return total;
+};
+
+console.log(searchLiteral(string));
+
+// вивести алфавіт
+
+
 
