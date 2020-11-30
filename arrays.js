7169ea693aaa523eb5a8cdb04af255fb8acf0258
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2020-11-30 23:59:30 +0200

    updated tasks

---
+++ arrays.js
@@ -0,0 +1,332 @@
+//TASK 1.
+// -=Сделать первый символ заглавным=-
+// Напишите функцию ucFirst(str), возвращающую строку str с заглавным первым символом. Например:
+// ucFirst("вася") == "Вася";
+
+// const str = 'ваCя';
+
+// const ucFirst = function(str) {
+//   return str[0].toUpperCase() + str.slice(1).toLowerCase();
+// };
+
+// ucFirst(str);
+
+//  TASK 2.
+// -= Операции с массивами =-
+// Давайте произведём 5 операций с массивом.
+// 1. Создайте массив styles с элементами «Джаз» и «Блюз».
+// 2. Добавьте «Рок-н-ролл» в конец.
+// 3. Замените значение в середине на «Классика». Ваш код для поиска значения в середине должен работать для массивов с любой длиной.
+// 4. Удалите первый элемент массива и покажите его.
+// 5. Вставьте «Рэп» и «Регги» в начало массива.
+
+// const styles = ['Джаз', 'Блюз'];
+
+//   styles.push('Рок-н-ролл');
+
+//   let index = Math.floor(styles.length / 2);
+
+//   console.log(styles);
+
+//   styles.splice(index, 1, 'Классика');
+//   console.log(styles);
+
+//   const deleted = styles.splice(0, 1);
+//   console.log(deleted);
+
+//   styles.splice(0, 0,'Рэп','Регги');
+//   console.log('styles :>> ', styles);
+
+// Task 3.
+// -=Сумма введённых чисел=-
+// Напишите функцию sumInput(), которая:
+// Просит пользователя ввести значения, используя prompt и сохраняет их в массив.
+// Заканчивает запрашивать значения, когда пользователь введёт не числовое значение, пустую строку или нажмёт «Отмена».
+// Подсчитывает и возвращает сумму элементов массива.
+// P.S. Ноль 0 – считается числом, не останавливайте ввод значений при вводе «0».
+
+// const sumInput = function(){
+
+//   const inputArr = [];
+//   let input = '';
+//   let sum = 0;
+
+//   do {
+//     input = prompt('Введіть значення');
+    
+//     if (Number.isNaN(Number(input))|| !input){
+//       break;
+//     }
+//     inputArr.push(input);
+//   } while(input !== null);
+
+
+//   for(let i = 0; i < inputArr.length; i+=1 ){
+//     sum += Number(inputArr[i]);
+//   }
+// }
+
+// sumInput();
+
+//================================== Task 4 ====================================================
+// -=Переведите текст вида border-left-width в borderLeftWidth=-
+// Напишите функцию camelize(str), которая преобразует строки вида «my-short-string» в «myShortString».
+// То есть дефисы удаляются, а все слова после них получают заглавную букву.
+// Примеры:
+// camelize("background-color") == 'backgroundColor';
+// camelize("list-style-image") == 'listStyleImage';
+// camelize("-webkit-transition") == 'WebkitTransition';
+// P.S. Подсказка: используйте split, чтобы разбить строку на массив символов, потом переделайте всё как нужно и методом join соедините обратно.
+//
+function camelize(str) {
+
+  const arr = str.split('-');
+  //console.log(arr);
+  for (const item of arr) {
+      if (arr.indexOf(item) === 0) {
+          continue
+      } 
+      const index = arr.indexOf(item);
+      const capitalize = item[0].toUpperCase() + item.slice(1, item.length);
+      arr.splice(index, 1, capitalize);
+      //item = item[0].toUpperCase() + item.slice(1, item.length);
+    
+      //console.log(item[0].toUpperCase() + item.slice(1, item.length));
+
+  }
+  return arr.join('')
+}
+const result = camelize('border-left-width');
+console.log(result);
+// ==============================================================================================
+// ========================================== Task 5 ============================================
+// -=Фильтрация по диапазону=-
+// Напишите функцию filterRange(arr, a, b), которая принимает массив arr, ищет в нём элементы между a и b и отдаёт массив этих элементов.
+// Функция должна возвращать новый массив и не изменять исходный.
+// Например:
+// let arr = [5, 3, 8, 1];
+// let filtered = filterRange(arr, 1, 4);
+// console.log( filtered ); // 3,1 (совпадающие значения)
+// console.log( arr ); // 5,3,8,1 (без изменений)
+
+const arr = [5, 3, 8, 1];
+
+const filterRange = (arr, a, b ) => {
+const newArr = [];
+
+  for (const num of arr) {
+    if(num >= a && num <= b){
+        newArr.push(num);
+    }
+  }
+    return newArr;
+}
+
+const result = filterRange(arr, 2, 4);
+console.log(result);
+// ==============================================================================================
+// ========================================== Task 6 ============================================
+// -=Фильтрация по диапазону "на месте"=-
+// Напишите функцию filterRangeInPlace(arr, a, b), которая принимает массив arr
+// и удаляет из него все значения кроме тех, которые находятся между a и b.
+// То есть, проверка имеет вид a ≤ arr[i] ≤ b.
+// Функция должна изменять принимаемый массив и ничего не возвращать.
+// Например:
+// let arr = [5, 3, 8, 1];
+// filterRangeInPlace(arr, 1, 4); // удалены числа вне диапазона 1..4
+// console.log( arr ); // [3, 1]
+//
+
+const arr = [5, 3, 8, 1];
+
+const filterRangeInPlace = function (arr, a, b) {
+ 
+  for (const num of arr) {
+    if (num < a || num > b) {
+      let index = arr.indexOf(num);
+      arr.splice(index, 1);
+    }
+  }
+  console.log(arr);
+};
+
+filterRangeInPlace(arr, 1, 4);
+// ==============================================================================================
+// ========================================== Task 7 ============================================
+// -=Отсортировать пользователей по возрасту=-
+// Напишите функцию sortByAge(users), которая принимает массив массивов и сортирует их по возрасту.
+// Например:
+// let vasya = [ "Вася", 25 ];
+// let petya = ["Петя", 30 ];
+// let masha = ["Маша", 28 ];
+// let arr = [ vasya, petya, masha ];
+// sortByAge(arr);
+//
+const vasya = ['Вася', 25];
+const petya = ['Петя', 30];
+const masha = ['Маша', 28];
+
+const arrOfNames = [vasya, petya, masha];
+
+   const newArray = [...arrOfNames];
+
+   newArray.sort(function sortByAge(a, b) {
+    return a[1] - b[1];
+  })
+
+ console.log(arrOfNames);
+ console.log(newArray);
+
+// ==============================================================================================
+// ========================================== Task 8 ============================================
+// -=Получить средний возраст=-
+// Напишите функцию getAverageAge(users), которая принимает массив объектов со свойством age и возвращает средний возраст.
+// Формула вычисления среднего арифметического значения: (age1 + age2 + ... + ageN) / N.
+// Например:
+// let vasya = [ "Вася", 25 ];
+// let petya = ["Петя", 30 ];
+// let masha = ["Маша", 29 ];
+// let arr = [ vasya, petya, masha ];
+// console.log( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
+
+const users = [
+  ['Вася', 25],
+  ['Петя', 38],
+  ['Маша', 29],
+];
+
+const age = [];
+let sum = 0;
+let result = 0;
+
+const getAverageAge = function (users) {
+  for (const user of users) {
+    age.push(user[1]);
+  }
+
+  for (const item of age) {
+    sum += item;
+  }
+  result = Math.round(sum / age.length);
+  return result;
+};
+
+console.log(getAverageAge(users));
+// ==============================================================================================
+// ========================================== Task 9 ============================================
+// -=Трансформировать в массив имён=-
+// У вас есть массив объектов user, и в каждом из них есть user.name. Напишите код, который преобразует их в массив имён.
+// Например:
+// let vasya = [ "Вася", 25 ];
+// let petya = ["Петя", 30 ];
+// let masha = ["Маша", 28 ];
+// let users = [ vasya, petya, masha ];
+// 
+
+const users = [
+  ['Вася', 25],
+  ['Петя', 38],
+  ['Маша', 29],
+];
+
+const names = [];
+// let sum = 0;
+// let result = 0;
+
+  for (const user of users) {
+    names.push(user[0]);
+  }
+
+  console.log(names);
+// ==============================================================================================
+// ========================================== Task 10 ===========================================
+// Нужно написать функцию, принимающую строку в качестве аргумента и возвращающую количество гласных, которые содержатся в строке.
+// Гласными являются «a», «e», «i», «o», «u».
+
+const str = 'A room without books is like a body without a soul';
+
+const getVowelLetter = function (str) {
+
+  const letterArr = str.toLowerCase().split('');
+  const vowelArr = [];
+
+  for (const letter of letterArr) {
+    
+    if (
+      letter === 'a' ||
+      letter === 'e' ||
+      letter === 'i' ||
+      letter === 'o' ||
+      letter === 'u'
+    ) {
+      vowelArr.push(letter);
+    }
+  }
+
+  let result = `Речення складається з ${vowelArr.length} голосних літер: ${vowelArr}`;
+  console.log(result);
+
+  return result;
+};
+
+getVowelLetter(str);
+// ==============================================================================================
+// ========================================== Task 11 ===========================================
+// -=Анаграмма=-
+// Так называют слово, которое содержит все буквы другого слова в том же количестве, но ином порядке.
+// Постановка
+// Нужно написать функцию, которая проверяет, являются ли две строки анаграммами, причем регистр букв не имеет значения. Учитываются лишь символы; пробелы или знаки препинания в расчет не берутся.
+// anagram('finder', 'Friend');
+//
+
+const anagram = function (firstWord, secondWord) {
+  
+  const normalizedFirstWord = firstWord
+    .replace(/[\s.,%]/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('');
+  const normalizedSecondWord = secondWord
+    .replace(/[\s.,%]/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('');
+  normalizedFirstWord === normalizedSecondWord
+    ? console.log(`${firstWord} and ${secondWord} is an anagram!`)
+    : console.log(`${firstWord} and ${secondWord} is not an anagram!`);
+
+};
+
+anagram('finder', 'Friend');
+
+// ==============================================================================================
+// ========================================== Task 12 ============================================
+// -=Палиндром=-
+// Палиндром — слово, предложение или последовательность символов,
+//  которая абсолютно одинаково читается как в привычном направлении,
+//  так и в обратном. К примеру, “Anna” — это палиндром, а “table” и “John” — нет.
+// Постановка
+// Дана строка; нужно написать функцию, которая позволяет вернуть значение true,
+//  если строка является палиндромом, и false — если нет.
+//  При этом нужно учитывать пробелы и знаки препинания.
+// palindrome("racecar") === true
+// palindrome("table") === false
+
+const palindrome = function (str) {
+  const normalizedStr = str.toLowerCase().replace(/\s/g, ''); // нормалізуємо рядок з урахуванням пробілів та приводимо до нижнього регістру
+
+  const reverseStr = normalizedStr.split('').reverse().join('');
+
+  if (normalizedStr === reverseStr) {
+    console.log(`String ${str} is a palindrome`);
+    return true;
+  } else {
+    console.log(`String ${str} is not a palindrome`);
+    return false;
+  }
+};
+
+palindrome('racecar');
+
--- objects.js
+++ objects.js
@@ -15,15 +15,12 @@
 // ];
 
 // const findFriendByName = function(allFriends, name){
-
 //   for (const friend of friends) {
 //     console.log(friend.name);
-
 //     if (friend.name === name){
 //       return 'Знайшли друга !!!';
 //     };
 //   };
-
 //   return 'Не знайшли :(';
 // };
 
@@ -35,12 +32,10 @@
 //   return names;
 // };
 
-// // console.log(findFriendByName(friends, 'Polly'));
-
+ // console.log(findFriendByName(friends, 'Polly'));
 // console.log(getAllNames(friends));
 
 // ===================================================================================
-
 //================================ Задача 1 ==========================================
 /* Есть объект salaries с произвольным количеством свойств, содержащих заработные платы.
 Напишите функцию sumSalaries(salaries), 
@@ -54,27 +49,20 @@
 // };
 
 // const sumSalaries = function(salaries){
-
 //   const values = Object.values(salaries);
-
 //   let sum = 0;
-
 //   for (const value of values) {
 //     sum += value;
 //   }
-
 //   return sum;
 // };
-
 // console.log(sumSalaries(salaries));
 // ===================================================================================
 
 //================================ Задача 2 ==========================================
 /*
 В базі даних користувачів стався збій. У деяких користувачів зникли значення з поля firstName, добре що у user вже є поле fullName, з якого ми можемо взяти потрібні дані. Давай повернено імена користувачам users у яких firstName рівне undefined за допомогою функції restoreNames. Повертати з функції нічого не потрібно.
-
 Приклад:
-
 const users = [
   ...,
   {
@@ -89,7 +77,6 @@ restoreNames(users)
  */
 
 // const users = [
-
 //   {
 //     name: undefined,
 //     lastName: 'Bennington',
@@ -100,8 +87,7 @@ restoreNames(users)
 //     lastName: 'Shinoda',
 //     fullName: 'Mike Shinoda',
 //   }
-
-// ]
+//]
 
 // const restoreNames = function(users) {
 
@@ -116,7 +102,6 @@ restoreNames(users)
 
 // restoreNames(users);
 // ========================================================================================
-
 //================================ Задача 3 ===============================================
 
 /*Ці дівчата... Сьогодні отримали колективний лист від усіх користувачів жіночої статі.
@@ -156,13 +141,11 @@ restoreNames(users)
 //   }
 //     return people;
 // }
-
 // console.log(removeFemaleAges(people));
 
 //=================================Second Method======================================================
 
 // function removeFemaleAges(people) {
-//   // write code here
 
 //   people.map(el => {
 //     if (el.gender === 'female') {
@@ -176,7 +159,6 @@ restoreNames(users)
 // removeFemaleAges(people);
 
 // ========================================================================================
-
 //================================ Задача 4 ===============================================
 
 /* На складах, порядок. Роботи стоять без роботи.
@@ -212,13 +194,10 @@ restoreNames(users)
 //     }
 
 //   } return numberOfBoxes;
-
 //   }
-
 //   countBoxes('aaaaca31');
 
 // ========================================================================================
-
 //================================ Задача 5 ===============================================
 /* Вночі у головного інженера зі столу хтось потягнув схему, за якою ми виготовляли наших роботів.
   Тепер ми не знаємо, які дані потрібні для робота, щоб він працював коректно. 
@@ -229,7 +208,6 @@ restoreNames(users)
   Результат має виглядати ось так: [[key, type], [key, type]...] */
 
 // Приклад:
-
 //   const robot = {
 //     version: 16,
 //     name: 'Cleaner 3000',
@@ -244,25 +222,220 @@ restoreNames(users)
 //  * @returns {string[][]}
 //  */
 
-      const robot = {
-        version: 16,
-        name: 'Cleaner 3000',
-        coords: [345, 12],
-      };
+      // const robot = {
+      //   version: 16,
+      //   name: 'Cleaner 3000',
+      //   coords: [345, 12],
+      // };
 
-      function getRobotSchema(robot) {
-        // write code here
-        const entries = Object.entries(robot); //[['version', 16], ['name', 'Cleaner 3000'], ['coords', [345, 12]]]
+      // function getRobotSchema(robot) {
+      //   // write code here
+      //   const entries = Object.entries(robot); //[['version', 16], ['name', 'Cleaner 3000'], ['coords', [345, 12]]]
          
-        for (const item of entries) {
+      //   for (const item of entries) {
 
-          item.splice(1, 1, (typeof item[1]));
-          // [['version', 'number'], ['name', 'string'], ['coords', 'object']]
-        }
+      //     item.splice(1, 1, (typeof item[1]));
+      //     // [['version', 'number'], ['name', 'string'], ['coords', 'object']]
+      //   }
 
-        return entries; 
-      }
+      //   return entries; 
+      // }
 
-      console.log(getRobotSchema(robot));
+      // console.log(getRobotSchema(robot));
 
+// ========================================================================================
+// Объекты
+// ==============================================================================
+// const products = [
+//   {
+//     id: '1',
+//     name: 'milk',
+//     value: 'Молоко',
+//     price: 20,
+//     sale: true,
+//     quantity: 10,
+//   },
+//   {
+//     id: '2',
+//     name: 'water',
+//     value: 'Вода',
+//     price: 10,
+//     sale: false,
+//     quantity: 50,
+//   },
+//   {
+//     id: '3',
+//     name: 'juice',
+//     value: 'Сок',
+//     price: 34,
+//     sale: true,
+//     quantity: 24,
+//   },
+//   {
+//     id: '4',
+//     name: 'fruitDrink',
+//     value: 'Морс',
+//     price: 32,
+//     sale: true,
+//     quantity: 25,
+//   },
+//   {
+//     id: '5',
+//     name: 'milkShake',
+//     value: 'Молочный коктейль',
+//     price: 37,
+//     sale: false,
+//     quantity: 37,
+//   },
+// ];
+
+//================================ Задача 1 ===============================================
+// 1. Создайте функцию getSaleProducts(), которая вернет новый массив со свойством sale равным true.
+
+// const getSaleProducts = function (array, prop) {
+//   const arr = [];
+//   for (const item of array) {
+//     if (item[prop]) {
+//       arr.push(item)
+//     }
+//   }
+//   return arr;
+// };
+
+// console.log(getSaleProducts(products,'sale'));
+
+// ========================================================================================
+//================================ Задача 2 ===============================================
+// 2. Создайте функцию getProductsName(), которая вернет новый массив со всеми названиями продуктов на русском языке.
+// const getProductsName = function (array) {
+//   const arr = [];
+//   for (const item of array) {
+//     arr.push(item.value);
+//   }
+//   return arr;
+// };
+// console.log(getProductsName(products));
+// ========================================================================================
+//================================ Задача 3 ===============================================
+// 3. Создайте функцию getTotalQuantity(), которая вернет общую сумму
+// всех товаров на складе (свойство quantity).
+
+// const getTotalQuantity = function (array) {
+// let total = 0;
+//   for (const item of array) {
+//     total+= item.quantity;
+//   }
+//   return total;
+// };
+
+// console.log(getTotalQuantity(products));
+
+// ========================================================================================
+//================================ Задача 4 ===============================================
+// 4. Создайте функцию getProductById(), которая будет возвращать продукт по id.
+
+// const getProductById = function (array, id) {
+//   for (const item of array) {
+//     if (item.id === id) {
+//       return item;
+//     }
+//   }
+// };
+
+// console.log(getProductById(products, '5'));
+// // ========================================================================================
+//================================ Задача 5 ===============================================
+// 5. Создайте функцию deleteProductById(), которая будет удялять из исходного массива products товар по id.
+
+// const deleteProductById = function (array, id) {
+//   for (const item of array) {
+//     if (item.id === id) {
+//       let index = array.indexOf(item);
+//       array.splice(index, 1);
+//     }
+//   }
+//   return array;
+// };
+
+// console.log(deleteProductById(products, '4'));
+// ========================================================================================
+//================================ Задача 6 ===============================================
+// 6. Создайте функцию createProduct(), которая будет добавлять новый продукт в исходный массив products.
+//  Товар передается в функцию в виде объекта
+
+// const createProduct = function (array, object) {
+//   array.push(object);
+//   return array;
+// };
+
+// console.log(createProduct(products, {
+//   id: '6',
+//   name: 'coffee',
+//   value: 'кофе',
+//   price: 40,
+//   sale: true,
+//   quantity: 45,
+// }));
+
+// ========================================================================================
+//================================ Задача 7 ===============================================
+// 7. Создайте функцию findProductByName(), которая будет возвращать объект из массива products
+// в зависимости от передаваемого значения name или value.
+
+// const{id, name, value, price, sale, quantity} = products
+
+// const findProductByName = (array, option) => {
+//   for (const item of array) {
+//     if (item.name === option || item.value === option) {
+//       return item;
+//     }
+//   }
+// };
+
+// console.log(findProductByName(products,'Сок'));
+// ========================================================================================
+//================================ Задача 8 ===============================================
+// 8. Создайте функцию editProduct(), которая будет изменять объект products. 
+// В качестве аргументов, в функцию будут передаваться id, а также ключ и его значение, которое нужно перезаписать.
+
+// const editProduct = (array, id, key, value) => {
+// for (const item of array) {
+// const {name} = products;
+//   if (item.id === id) {
+//     item[key] = value;
+//   }
+// }
+// return products;
+// }
+
+// console.log(editProduct(products, '5', 'name', 'milkPleasure'));
+// ========================================================================================
+//================================ Задача 9 ===============================================
+// 9. Создайте функцию modifyProductList(), которая будет возвращать новый массив с продуктами
+//  у которых будут только свойства id, name, value и price
+
+// const modifyProductList = (arr) =>{
+//   const newArr = [];
+//   for (const item of arr) {
+//     const {id, name, value, price} = item;
+//     newArr.push({id, name, value, price});
+//   }
+//   return newArr;
+// };
+// console.log(modifyProductList(products));
+// ========================================================================================
+//================================ Задача 10 ===============================================
+// 10. Создайте функцию addToCart(), которая будет записывать товар по id в новый массив cart.
+
+// const addToCart =  function (array, id) {
+// const cart =[];
+// for (const item of array) {
+//   if(item.id === id) {
+//     cart.push(item);
+//   }
+// }
+// return cart;
+// }
+
+// console.log(addToCart(products, '5'));
 // ========================================================================================
